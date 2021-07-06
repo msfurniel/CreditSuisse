4cccbc7ac9d3a5bfad4223d60338219bbeae3b5f
msfurniel <87031592+msfurniel@users.noreply.github.com>
2021-07-06 13:42:20 -0300

    Initial commit

---
+++ Interface/ITrade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreditSuisse.Interface
+{
+    interface ITrade
+    {
+        double Value { get; }
+        string ClientSector { get; }
+        DateTime NextPaymentDate { get; }
+    }
+}
---
+++ Model/TradeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreditSuisse.Model
+{
+    interface ITrade
+    {
+        double Value { get; }
+        string ClientSector { get; }
+        DateTime NextPaymentDate { get; }
+    }
+
+    public class TradeModel : ITrade
+    {
+        public DateTime referenceDate { get; set; }
+        public Int32 tradesQuantity { get; set; }
+        public double Value { get; set; }
+        public string ClientSector { get; set; }
+        public DateTime NextPaymentDate { get; set; }
+    }
+}
